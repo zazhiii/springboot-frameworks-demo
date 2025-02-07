e44ec0e1696c6cb603aebeb3338493f4f4415bf5
zazhi <2065707071@qq.com>
2025-02-07 14:33:35 +0800

    refactor: 更新项目名称

---
+++ springboot_shiro_jwt/src/main/java/com/zazhi/shiro_demo/service/UserService.java
@@ -0,0 +1,37 @@
+package com.zazhi.shiro_demo.service;
+
+import com.zazhi.shiro_demo.common.JwtUtil;
+import com.zazhi.shiro_demo.pojo.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author zazhi
+ * @date 2024/12/9
+ * @description: TODO
+ */
+@Service
+public class UserService {
+    public String login(String username, String password) {
+        // 判断逻辑省略
+        return JwtUtil.genToken(Map.of("username", username));
+    }
+
+    public Set<String> findPermissionsByUsername(String username) {
+        return Set.of("user:delete", "user:update");
+    }
+
+    public Set<String> findRolesByUsername(String username) {
+        // 模拟从数据库中查询用户角色
+        if(username.equals("admin")){
+            return Set.of("admin");
+        }
+        return Set.of("user");
+    }
+
+    public String getUsernameById(String userId) {
+        return "zhangsan";
+    }
+}
---
+++ springboot_shiro_jwt/src/test/java/com/zazhi/shiro_demo/ShiroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zazhi.shiro_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
